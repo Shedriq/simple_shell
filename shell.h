ea330b899df70474f0937ac24979bf3f131a4712
Shedriq <jerrydwain4@gmail.com>
2022-09-07 14:51:58 +0300

    Header file

--- shell.h
+++ shell.h
@@ -1,61 +1,79 @@
-#include "shell.h"
+#ifndef SHELL_H
+#define SHELL_H
 
+/* STD LIBRARIES */
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/wait.h>
+#include <errno.h>
+#include <stddef.h>
+#include <unistd.h>
+#include <signal.h>
 
-	char **commands = NULL;
-	char *line = NULL;
-	char *shell_name = NULL;
-	int status = 0;
+/*---------------------------------------------------------*/
+
+/* MACROS */
+#define SIGINT 2
+extern char **environ;
+extern int errno;
+
+/*---------------------------------------------------------*/
+
+/* PROTOTYPES */
+
+/*-----Main functions-----*/
+int shell_interactive(void);
+int shell_no_interactive(int ac, char **av);
+
+/*-----String functions-----*/
+int _strlen(char *string);
+int _strcmp(char *s1, char *s2);
+char *_strncmp(char *PATH, char *MATCH, size_t n);
+int count_words(char *str);
+char *_strdup(char *str);
+char *_strcat(char *dest, char *src);
+char *_itoa(int i, char *strout, int base);
+
+/*-----Aux functions-----*/
+char **tokenizer(char *s, char *delim);
+int (*get_builtin(char *string))();
+int fork_process(char *path, char **tokens, char **environ);
+char *find_path(char **tokens);
+char *prompt_no_interactive(char *name_file);
+char *prompt_interactive(char *name_file);
+void handle_sigint(int sig);
+void free_all(char **tokens);
+
+/*-----Error_messages-----*/
+void path_error(int numerr, char **tokens, char *name_file);
+void permission_error(int numerr, char **tokens, char *name_file);
+
+/*-----Built-ins-----*/
+int _own_exit(void);
+int env(void);
+
+/*---------------------------------------------------------*/
+
+/*STRUCTURES*/
 
 /**
- * main - the main shell code
- * @argc: number of arguments passed
- * @argv: program arguments to be parsed
- *
- * applies the functions in utils and helpers
- * implements EOF
- * Prints error on Failure
- * Return: 0 on success
- */
+*struct builtin - builtin structures.
+*@name: a char with a name of a file.
+*@func: a int that call.
+*/
 
-
-int main(int argc __attribute__((unused)), char **argv)
+typedef struct builtin
 {
-	char **current_command = NULL;
-	int i, type_command = 0;
-	size_t n = 0;
+	char *name;
+	int (*func)();
+} built_in;
 
-	signal(SIGINT, ctrl_c_handler);
-	shell_name = argv[0];
-	while (1)
-	{
-		non_interactive();
-		print(" ($) ", STDOUT_FILENO);
-		if (getline(&line, &n, stdin) == -1)
-		{
-			free(line);
-			exit(status);
-		}
-			remove_newline(line);
-			remove_comment(line);
-			commands = tokenizer(line, ";");
+static const built_in list[] = {
+	{"env", env},
+	{"exit", _own_exit},
+	{NULL, NULL}};
 
-		for (i = 0; commands[i] != NULL; i++)
-		{
-			current_command = tokenizer(commands[i], " ");
-			if (current_command[0] == NULL)
-			{
-				free(current_command);
-				break;
-			}
-			type_command = parse_command(current_command[0]);
 
-			/* initializer -   */
-			initializer(current_command, type_command);
-			free(current_command);
-		}
-		free(commands);
-	}
-	free(line);
-
-	return (status);
-}
+#endif /* SHELL.H */
